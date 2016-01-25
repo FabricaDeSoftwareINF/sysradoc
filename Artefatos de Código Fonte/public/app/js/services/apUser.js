2a23aa2f56eb7486509da0d0f900bca0e79b4634
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-01-25 20:08:00 -0200

    Arrumando arquivos para preparar a implementação de casos de uso

--- Artefatos de Código Fonte/public/app/js/services/apUser.js
+++
@@ -1,7 +0,0 @@
-angular.module('app').factory('apUser', function ($resource) {
-    var UserResource = $resource('/api/users/:id/:email/:token', {_id: "@id", email: "@email", token: "@token"}, {
-        update: {method: 'PUT', isArray: false}
-    });
-
-    return UserResource;
-});
