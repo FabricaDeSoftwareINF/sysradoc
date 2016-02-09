c0699be8bb339de7660781039ae5330d4812b2b9
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-02-09 17:12:03 -0200

    Realizando ajustes na view, controller e model de Processo

--- SysRadoc/public/app/js/services/ngUser.js
+++ SysRadoc/public/app/js/services/ngUser.js
@@ -1,5 +1,5 @@
 angular.module('app').factory('ngUser', function ($resource) {
-    var UserResource = $resource('/api/users/:id/:email/:token', {id: "@id", email: "@email", token: "@token"}, {
+    var UserResource = $resource('/api/users/:id/:email/:token/:category', {id: "@id", email: "@email", token: "@token", category: "@category"}, {
         update: {method: 'PUT', isArray: false}
     });
 
