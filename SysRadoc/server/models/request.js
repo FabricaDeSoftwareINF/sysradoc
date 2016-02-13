bf7a68a12edef872ff21f132c25fce23a75a9e3f
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-02-13 03:00:37 -0200

    Adicionando model de Solicitacao

---
+++ SysRadoc/server/models/request.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+module.exports = function () {
+
+    var requestSchema = mongoose.Schema({
+        idUsuario: mongoose.Schema.Types.ObjectId,
+        nomeUsuario: String,
+        tipo: String
+    });
+
+    return mongoose.model('Request', processSchema);
+};
