2f982d7ab9bf3fe5aea0119ed976b42543e7ff89
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-02-14 22:11:17 -0200

    Adicionando models de quadro sumario e pontuacao de radoc, e arrumando service de pontuacao de radoc

---
+++ SysRadoc/server/models/radocScore.js
@@ -0,0 +1,28 @@
+ var radocParseConfig = require("./../services/radocParseConfig");
+ var mongoose = require('mongoose');
+
+ module.exports = function () {
+
+     var radocScoreSchemaJSON = {
+         idRadoc: { type: mongoose.Schema.ObjectId, ref: 'Radoc' },
+         idProfessor: { type: mongoose.Schema.ObjectId, ref: 'User' },
+
+     };
+
+     for (var s = 0; s < radocParseConfig.sections.length; s++){
+         var section = radocParseConfig.sections[s];
+         radocScoreSchemaJSON[section.header] = [{
+            secao: String,
+            subsecao: String,
+            subitem: String,
+            pontuacao: Number,
+            modificacaoSoftware: String,
+            novaPontuacao: Number,
+            modificacao: String
+         }];
+     }
+
+    var radocScoreSchema = mongoose.Schema(radocScoreSchemaJSON);
+
+     return mongoose.model('RadocScore', radocScoreSchema);
+ };
