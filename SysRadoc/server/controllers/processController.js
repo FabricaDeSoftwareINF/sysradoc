c8f015832b4f313e6870f9a44b792552294f3de9
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-02-22 16:16:32 -0300

    Adicionando checagem de data do processo

--- SysRadoc/server/controllers/processController.js
+++ SysRadoc/server/controllers/processController.js
@@ -26,7 +26,7 @@ module.exports = function(app){
         Process.count({idProfessor: processData.idProfessor, situacao: "ABERTO"}, function(err, countProcess){
             if (countProcess === 0){
                 if (processData.tipo !== "Estágio Probatório"){
-                    Request.count({idUsuario: processData.idProfessor, tipo: processData.tipo}, function(err, countRequest){
+                    Request.count({idUsuario: processData.idProfessor, tipo: processData.tipo, dataDeInicio: processData.dataDeInicio, dataFim: processData.dataFim}, function(err, countRequest){
                         if (countRequest > 0){
                             Process.create(req.body, function(err, process){
                                 Request.remove({idUsuario: processData.idProfessor, tipo: processData.tipo}).exec(function(err, collection){
@@ -36,7 +36,7 @@ module.exports = function(app){
                             });
                         }
                         else{
-                            res.send({reason: "Não existe nenhuma solicitação de " + processData.tipo + " para este docente."});
+                            res.send({reason: "Não existe nenhuma solicitação de " + processData.tipo + " neste período para este docente."});
                         }
                     });
 
