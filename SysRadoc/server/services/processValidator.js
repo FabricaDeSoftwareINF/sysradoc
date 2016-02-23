15d7690274be475d4637cc031306fb0c1f26664e
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-02-23 02:10:43 -0300

    Adicionando validação de solicitações

--- SysRadoc/server/services/processValidator.js
+++ SysRadoc/server/services/processValidator.js
@@ -1,20 +1,5 @@
 module.exports = function(app){
 
-    var classes = [
-        "A",
-        "B",
-        "C",
-        "D",
-        "E"
-    ];
-    var levels = {
-        A: ["1", "2"],
-        B: ["1", "2"],
-        C: ["1", "2", "3", "4"],
-        D: ["1", "2", "3", "4"],
-        E: ["1"]
-    };
-
 	var Process = app.models.process,
         Request = app.models.request,
         User = app.models.user;
@@ -22,9 +7,20 @@ module.exports = function(app){
 	var service = {};
 
     service.validateAndCreate = function(processData, callback){
-        Request.count({idUsuario: processData.idProfessor, tipo: processData.tipo, dataDeInicio: processData.dataDeInicio, dataFim: processData.dataFim}, function(err, countRequest){
-            if (countRequest > 0){
-                Process.create(req.body, function(err, process){
+        Request.findOne({idUsuario: processData.idProfessor, tipo: processData.tipo}, function(err, requestDoc){
+
+            if (requestDoc){
+                var dataInicioA = new Date(processData.dataDeInicio), dataInicioB = new Date(requestDoc.dataDeInicio);
+                var dataFimA = new Date(processData.dataFim), dataFimB = new Date(requestDoc.dataFim);
+                if (
+                    dataInicioA.getMonth() !== dataInicioB.getMonth() ||
+                    dataInicioA.getFullYear() !== dataInicioB.getFullYear() ||
+                    dataFimA.getMonth() !== dataFimB.getMonth() ||
+                    dataFimA.getFullYear() !== dataFimB.getFullYear()){
+                        callback({reason: "As datas do processo não coincidem com a da solicitação."});
+                        return;
+                    }
+                Process.create(processData, function(err, process){
                     Request.remove({idUsuario: processData.idProfessor, tipo: processData.tipo}).exec(function(err, collection){
                         callback({success: true});
                     });
@@ -32,7 +28,7 @@ module.exports = function(app){
                 });
             }
             else{
-                callback({reason: "Não existe nenhuma solicitação de " + processData.tipo + " neste período para este docente."});
+                callback({reason: "Não existe nenhuma solicitação de " + processData.tipo + " para este docente."});
             }
         });
     };
---
+++ SysRadoc/server/services/requestValidator.js
@@ -0,0 +1,78 @@
+module.exports = function(app){
+
+    function diffMonths(dateA, dateB){
+        var months;
+        months = (dateB.getFullYear() - dateA.getFullYear()) * 12;
+        months -= dateA.getMonth();
+        months += dateB.getMonth();
+        return months <= 0 ? 0 : months;
+    }
+
+    var classes = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E"
+    ];
+    var levels = {
+        A: ["1", "2"],
+        B: ["1", "2"],
+        C: ["1", "2", "3", "4"],
+        D: ["1", "2", "3", "4"],
+        E: ["1"]
+    };
+
+	var Process = app.models.process,
+        Request = app.models.request,
+        User = app.models.user;
+
+	var service = {};
+
+    service.validateAndCreate = function(requestData, callback){
+        Request.count({idUsuario: requestData.idUsuario}, function(err, countReq){
+            if (countReq > 0){
+                callback({reason: "Não é possível realizar mais de uma solicitação por vêz."});
+            }
+            else{
+                User.findOne({_id: requestData.idUsuario}).exec(function(err, user){
+                    var lastChangeMonths = diffMonths(user.dataEntradaUltimoNivel, new Date());
+                    if (requestData.tipo === "Progressão Funcional"){
+                        if (levels[user.classe].indexOf(user.nivel) === levels[user.classe].length - 1){
+                            callback({reason: "Não é possível pedir uma progressão, pois você já se encontra no último nível de sua classe."});
+                            return false;
+                        }
+                        else if (lastChangeMonths < 24){
+                            callback({reason: "Não é possível pedir uma progressão, pois você não cumpriu ainda um total de 24 meses no seu nível atual."});
+                            return false;
+                        }
+
+                    }
+                    else if (requestData.tipo === "Promoção na Carreira de Magistério"){
+                        if (levels[user.classe].indexOf(user.nivel) !== levels[user.classe].length - 1){
+                            callback({reason: "Não é possível pedir uma promoção, pois você não se encontra no último nível de sua classe."});
+                            return false;
+                        }
+                        else if (user.classe === "E"){
+                            callback({reason: "Não é possível pedir uma promoção, pois você já se encontra na última classe."});
+                            return false;
+                        }
+                        else if (!user.estagioProbatorioCompleto){
+                            callback({reason: "Não é possível pedir uma promoção com estágio probatório incompleto."});
+                            return false;
+                        }
+                        else if (lastChangeMonths < 48){
+                            callback({reason: "Não é possível pedir uma promoção pois você não cumpriu ainda um total de 2 anos no último nível de sua classe."});
+                            return false;
+                        }
+                    }
+                    Request.create(requestData, function(err, request){
+                        callback({success: true});
+                    });
+                });
+            }
+        });
+    };
+
+	return service;
+};
